b05cfce77acfd8fc7dd567b67e82de8b0de27ac5
Vasilis Ntranos <vasilis@213.local>
2023-04-15 14:26:56 -0700

    clean up

--- run.sh
+++ run.sh
@@ -6,17 +6,3 @@ osascript -e 'tell application "Terminal" to activate'
 while pgrep -q -s "$TERMINAL_PID"; do
     sleep 1
 done
-
-# TERMINAL_PID=$(osascript -e 'tell application "Terminal" to do script "cd '$PWD' && source venv/bin/activate && python application.py && deactivate && echo \"\nPress [Cmd + W] or [Cmd + Q] to close the Terminal window.\" && bash"')
-# osascript -e 'tell application "Terminal" to activate'
-
-# while pgrep -q -s "$TERMINAL_PID"; do
-#     sleep 1
-# done
-
-
-# export LC_ALL=en_US.UTF-8
-# export LANG=en_US.UTF-8
-
-# source venv/bin/activate
-# python3 application.py
